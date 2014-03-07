992ad7ac43f39e9fbfc527815c02dad7a4e2a925
ZipionLive <alexandre@zipion-live.be>
2014-03-07 18:06:14 +0100

    Malfunctionning code added and commented

--- functions.php
+++ functions.php
@@ -8,4 +8,22 @@
         }
         return;
     }
+    
+   // function importStyleSheet($file) {
+   //     if ($file != "") {
+   //         echo('<link rel="stylesheet" href="');
+   //         getPath($stylesPath, $file);
+   //         echo('" type="text/css" media="screen" />');
+   //     }
+   //     return;
+   //}
+   // 
+   // function importScript($file) {
+   //     if ($file != "") {
+   //         echo('<script type="text/javascript" src="');
+   //         getPath($scriptsPath, $file);
+   //         echo('"></script>');
+   //     }
+   //     return;
+   // }
 ?>
